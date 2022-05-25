b18d4268c573bc6559051d80885e2031569109e3
Antonio Ruggiero <antonrugg@gmail.com>
2022-05-25 14:51:32 +0200

    added Comic model - added Comic controller -r - added table structure - run migration

--- database/migrations/2022_05_25_121900_create_comics_table.php
+++ database/migrations/2022_05_25_121900_create_comics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateComicTable extends Migration
+class CreateComicsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,16 @@ class CreateComicTable extends Migration
      */
     public function up()
     {
-        Schema::create('comic', function (Blueprint $table) {
+        Schema::create('comics', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title', 200);
+            $table->text('description');
+            $table->string('thumb');
+            $table->float('price', 8, 2);
+            $table->string('series', 100);
+            $table->date('sale_date');
+            $table->string('type', 100);
         });
     }
 
@@ -26,6 +33,6 @@ class CreateComicTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('comic');
+        Schema::dropIfExists('comics');
     }
 }
